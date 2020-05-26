c00403037892d8982528f80d23c1c35e40b5f3e0
WangFreya <wangfreya097@gmail.com>
2020-05-26 17:27:49 -0400

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,107 @@
+
+public class Question {
+	
+	private int num1;
+	private int num2;
+	private int answer;
+	private int qType;
+	private String quest;
+	
+	public Question() {
+		this.num1 = 0;
+		this.num2 = 0;
+		this.qType = 0;	
+	}
+		
+	public Question(int n1, int n2, int qT) {
+		this.num1 = Math.max(n1, n2);
+		this.num2 = Math.min(n1, n2);
+		this.qType = qT;
+		if (n1 == n2 && qT == 2) {
+			this.answer = 0;
+		} else {
+			this.setAnswer(this.qType);
+		}
+		this.setQuestion(this.qType);
+	}
+	
+	public String getQuest() {
+		return this.quest;
+	}
+	
+	public int getAns() {
+		return this.answer;
+	}
+	
+	public void setNum1(int num) {
+		this.num1 = num;
+	}
+	
+	public void setNum2(int num) {
+		this.num2 = num;
+	}
+	
+	public int getDivAns(int n1, int n2) {
+		int ans = 0;
+		int num1 = n1;
+		int num2 = n2;
+		
+		int remainder = num1 % num2;
+		if (remainder == 0) {
+			ans = num1 / num2;
+		} else {
+			int fact = 2;
+			int mult = fact * num2;
+			while (mult < num1) {
+				fact += 1;
+				mult = fact * num2;
+			}
+			setNum1(mult);
+			setNum2(num2);
+			ans = fact;
+		}
+		return ans;
+	}
+	
+	public void setAnswer(int qT) {
+		switch (qT) {
+		case 1:
+			this.answer = this.num1 + this.num2;
+			break;
+		case 2:
+			this.answer = this.num1 - this.num2;
+			break;
+		case 3:
+			this.answer = this.num1 * this.num2;
+			break;
+		case 4:
+			this.answer = getDivAns(this.num1, this.num2);
+			break;
+		}
+	}
+	
+	public void setQuestion(int qT) {
+		switch (qT) {
+		case 1:
+			this.quest = this.num1 + " + " + this.num2 + " = __ ? ";
+			break;
+		case 2:
+			this.quest = this.num1 + " - " + this.num1 + " = __ ? ";
+			break;
+		case 3:
+			this.quest = this.num1 + " x " + this.num2 + " = __ ? ";
+			break;
+		case 4:
+			this.quest = this.num1 + " / " + this.num2 + " = __ ? ";
+			break;
+		}
+	}
+	
+	public boolean checkQ(int ans) {
+		if (ans == this.answer) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
