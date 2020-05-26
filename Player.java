57645ff2039d94bad92dde19024b88eec6f1b52d
WangFreya <wangfreya097@gmail.com>
2020-05-26 17:26:07 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.*;
+
+
+public class Player {
+	
+	private int hitPoints;
+	
+	public Player() {
+		this.hitPoints = 0;
+	}
+	
+	public Player(int hp) {
+		this.hitPoints = hp;
+	}
+	
+	public int getHP() {
+		return hitPoints;
+	}
+	
+	public void setHP(int hP) {
+		this.hitPoints = hP;
+	}
+	
+	/*public int rando(int upper, int check) {
+		Random rand = new Random();
+		int rand1 = rand.nextInt(upper) + check; 
+		return rand1;
+	}*/
+	
+}
