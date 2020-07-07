51377c136dc2a7fdf7bac16a497e77dacfdfe22a
WangFreya <wangfreya097@gmail.com>
2020-07-07 10:23:57 -0400

    Update Question.java
    Slightly updated file

--- Question.java
+++ Question.java
@@ -15,11 +15,12 @@ public class Question {
 		this.quest = "No question";
 	}
 		
-	public Question(int n1, int n2, int answer, int qType) {
+	public Question(int n1, int n2, int answer, int qTyp) {
 		this.num1 = n1;
 		this.num2 = n2;
 		this.answer = answer;
-		this.setQuestion(qType);
+		this.qType = qTyp;
+		this.setQuestion(this.qType);
 	}
 	
 	public String getQuest() {
@@ -37,50 +38,7 @@ public class Question {
 	public void setNum2(int num) {
 		this.num2 = num;
 	}
-	
-	/*public int getDivAns(int n1, int n2) {
-		int ans = 0;
-		int num1 = n1;
-		int num2 = n2;
 		
-		int remainder = num1 % num2;
-		if (remainder == 0) {
-			ans = num1 / num2;
-		} else {
-			int fact = 2;
-			int mult = fact * num2;
-			while (mult < num1) {
-				fact += 1;
-				mult = fact * num2;
-			}
-			setNum1(mult);
-			setNum2(num2);
-			ans = fact;
-		}
-		return ans;
-	}*/
-	
-	/*public void setAnswer(int qT) {
-		switch (qT) {
-		case 1:
-			this.answer = this.num1 + this.num2;
-			break;
-		case 2:
-			if (this.num1 == this.num2) {
-				this.answer = 0;
-			} else {
-				this.answer = this.num1 - this.num2;
-			}
-			break;
-		case 3:
-			this.answer = this.num1 * this.num2;
-			break;
-		case 4:
-			this.answer = getDivAns(this.num1, this.num2);
-			break;
-		}
-	}*/
-	
 	public void setQuestion(int qT) {
 		switch (qT) {
 		case 1:
